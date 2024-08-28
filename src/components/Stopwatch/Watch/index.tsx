304aebbe6f9187b681e4333a9862c0da350fbd62
CezarDev <bacanieskidev@gmail.com>
2024-08-28 10:36:40 -0300

    feat: criacao de components watch e stopwatch

---
+++ src/components/Stopwatch/Watch/index.tsx
@@ -0,0 +1,9 @@
+export default function Watch() {
+  <>
+    <span>0</span>
+    <span>0</span>
+    <span>:</span>
+    <span>0</span>
+    <span>0</span>
+  </>;
+}
---
+++ src/components/Stopwatch/index.tsx
@@ -0,0 +1,13 @@
+import Button from '../Button';
+
+export default function Stopwatch() {
+  return (
+    <>
+      <p>Choosea card and start the stopwatch</p>
+
+      <div></div>
+
+      <Button text='Start' />
+    </>
+  );
+}
