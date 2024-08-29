d71395db137b74504d7553d9cc4b73a7f0270ac5
CezarDev <bacanieskidev@gmail.com>
2024-08-29 17:21:36 -0300

    feat: fazendo update do cronometro com useEffect

--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -1,16 +1,30 @@
 import Button from '../Button';
 import Watch from './Watch';
 import style from './Stopwatch.module.css';
+import { ITask } from '../../types/ITask';
+import { useEffect, useState } from 'react';
+import { timeToSeconds } from '../../common/utils/time';
 
-export default function Stopwatch() {
+interface StopwatchProps {
+  selected: ITask | undefined;
+}
+
+export default function Stopwatch({ selected }: StopwatchProps) {
+  const [time, setTime] = useState<number>();
+  
+  useEffect(() => {
+    if (selected?.time) {
+      setTime(timeToSeconds(selected.time));
+    }
+  }, [selected]);
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Choose a card and start the stopwatch</p>
-
+      Time: {time}
       <div className={style.relogioWrapper}>
         <Watch />
       </div>
-
       <Button text='Start' />
     </div>
   );
